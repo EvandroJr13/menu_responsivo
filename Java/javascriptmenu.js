b8d3b5f18ea6616c03ea162c88467cb32ebe766b
Evandro <evandro.asjunior13@gmail.com>
2023-04-27 17:04:09 -0300

    Atualizado 27.04

--- Java/javascriptmenu.js
+++ Java/javascriptmenu.js
@@ -1,1 +1,15 @@
-const menuMobile = document.querySelector("#menu");
+const menu = document.querySelector("#menu");
+const menu_mobile = document.querySelector(".menu_mobile")
+
+menu.style.maxHeight = "0px";
+
+menu_mobile.addEventListener("click",()=>{
+    if(menu.style.maxHeight == "0px")
+    menu.style.maxHeight = "150px"
+    else
+    menu.style.maxHeight = "0px"
+})
+
+let logo = document.querySelector("#logo")
+let home = document.querySelector("#home")
+
